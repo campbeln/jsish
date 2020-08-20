ca9edf4a5bf541cf747efd9a92d05b77b8f07e61
campbeln <campbeln@gmail.com>
2020-08-19 23:57:08 -0700

    Update docs and fix stupid error in type-ex

--- ish.js
+++ ish.js
@@ -21,7 +21,7 @@
  *      All features are organized in individually includable mixins organized by namespace/major features with only the core <code>ish.js</code> functionality required to bootstrap.
  *  </p>
  * </div>
- * @version 0.13.2020-08-04
+ * @version 0.13.2020-08-12
  * @author Nick Campbell
  * @license MIT
  * @copyright 2014-2020, Nick Campbell
@@ -46,7 +46,7 @@
         oPrivate = {},
         oTypeIsIsh = { //# Set the .ver and .target under .type.is.ish (done here so it's at the top of the file for easy editing) then stub out the .app and .lib with a new .pub oInterfaces for each
             config: {
-                ver: '0.13.2020-08-04',
+                ver: '0.13.2020-08-12',
                 onServer: bServerside,
                 debug: true,
                 //script: _undefined,
